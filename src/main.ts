b74de49b8cb3bafe1e5ac3753867d17b608826cd
Michael Prentice <splaktar@gmail.com>
2020-03-28 23:19:08 -0400

    feat: add logrocket

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as LogRocket from 'logrocket';
 
 if (environment.production) {
   enableProdMode();
+  LogRocket.init('xltsdev/angularjsxltsdev');
 }
 
 platformBrowserDynamic()
