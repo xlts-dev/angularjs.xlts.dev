dbbf9626b6596da142851aad5f2d16b3351d5f49
Michael Prentice <splaktar@gmail.com>
2020-04-03 10:55:22 -0400

    fix(landing, contact-form): improve responsiveness

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-contact-form',
@@ -6,7 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.scss'],
 })
 export class ContactFormComponent implements OnInit {
-  constructor() {}
+  iFrameWidth: Observable<number>;
+
+  constructor(breakpointObserver: BreakpointObserver) {
+    this.iFrameWidth = combineLatest([
+      breakpointObserver.observe(['(max-width: 369.99px)']), // Under 370px
+      breakpointObserver.observe(['(min-width: 400px)']), // Over 400px
+      breakpointObserver.observe(['(min-width: 640px)']), // Over 640px
+    ]).pipe(
+      map(([under370, over400, over640]) => {
+        if (under370.matches) {
+          return 300;
+        }
+        if (over640.matches) {
+          return 600;
+        }
+        if (over400.matches) {
+          return 380;
+        }
+        return 350;
+      }),
+    );
+  }
 
   ngOnInit(): void {}
 }
